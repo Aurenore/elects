2e850baef5512a010342910eaf1056d81c2bd68a
Anya M <anya-aurore.mauron@epfl.ch>
2024-03-26 18:41:51 +0100

    harmonic mean works locally

--- train.py
+++ train.py
@@ -8,49 +8,14 @@ from earlyrnn import EarlyRNN
 import torch
 from tqdm import tqdm
 from loss import EarlyRewardLoss
-import numpy as np
 import sklearn.metrics
 import pandas as pd
-import argparse
-import os
 import wandb
-from utils.plots import plot_label_distribution_datasets, boxplot_stopping_times, plot_boxplot
-from utils.doy import get_doys_dict_test, get_approximated_doys_dict, get_doy_stop, create_sorted_doys_dict_test
+from utils.plots import plot_label_distribution_datasets, boxplot_stopping_times
+from utils.doy import get_doys_dict_test, get_doy_stop, create_sorted_doys_dict_test, get_approximated_doys_dict
+from utils.helpers_training import parse_args, train_epoch, test_epoch
+from utils.metrics import harmonic_mean_score
 import matplotlib.pyplot as plt
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Run ELECTS Early Classification training on the BavarianCrops dataset.')
-    parser.add_argument('--dataset', type=str, default="bavariancrops", choices=["bavariancrops","breizhcrops", "ghana", "southsudan","unitedstates"], help="dataset")
-    parser.add_argument('--alpha', type=float, default=0.5, help="trade-off parameter of earliness and accuracy (eq 6): "
-                                                                 "1=full weight on accuracy; 0=full weight on earliness")
-    parser.add_argument('--epsilon', type=float, default=10, help="additive smoothing parameter that helps the "
-                                                                  "model recover from too early classificaitons (eq 7)")
-    parser.add_argument('--learning-rate', type=float, default=1e-3, help="Optimizer learning rate")
-    parser.add_argument('--weight-decay', type=float, default=0, help="weight_decay")
-    parser.add_argument('--patience', type=int, default=30, help="Early stopping patience")
-    parser.add_argument('--device', type=str, default="cuda" if torch.cuda.is_available() else "cpu",
-                        choices=["cuda", "cpu"], help="'cuda' (GPU) or 'cpu' device to run the code. "
-                                                     "defaults to 'cuda' if GPU is available, otherwise 'cpu'")
-    parser.add_argument('--epochs', type=int, default=100, help="number of training epochs")
-    parser.add_argument('--sequencelength', type=int, default=70, help="sequencelength of the time series. If samples are shorter, "
-                                                                "they are zero-padded until this length; "
-                                                                "if samples are longer, they will be undersampled")
-    parser.add_argument('--batchsize', type=int, default=256, help="number of samples per batch")
-    parser.add_argument('--dataroot', type=str, default=os.path.join(os.environ.get("HOME", os.environ.get("USERPROFILE")),"elects_data"), help="directory to download the "
-                                                                                 "BavarianCrops dataset (400MB)."
-                                                                                 "Defaults to home directory.")
-    parser.add_argument('--snapshot', type=str, default="snapshots/model.pth",
-                        help="pytorch state dict snapshot file")
-    parser.add_argument('--resume', action='store_true')
-
-
-    args = parser.parse_args()
-
-    if args.patience < 0:
-        args.patience = None
-
-    return args
 
 def main(args):
     # ----------------------------- LOAD DATASET -----------------------------
@@ -193,6 +158,7 @@ def main(args):
             classification_loss = stats["classification_loss"].mean()
             earliness_reward = stats["earliness_reward"].mean()
             earliness = 1 - (stats["t_stop"].mean() / (args.sequencelength - 1))
+            harmonic_mean = harmonic_mean_score(accuracy, stats["classification_earliness"])
 
             # ----------------------------- LOGGING -----------------------------
             train_stats.append(
@@ -205,31 +171,34 @@ def main(args):
                     recall=recall,
                     fscore=fscore,
                     kappa=kappa,
-                    earliness=earliness,
+                    elects_earliness=earliness,
                     classification_loss=classification_loss,
-                    earliness_reward=earliness_reward
+                    earliness_reward=earliness_reward,
+                    classification_earliness=stats["classification_earliness"],
+                    harmonic_mean=harmonic_mean,
                 )
             )
             fig_boxplot, ax_boxplot = plt.subplots(figsize=(15, 7))
             doys_dict = get_approximated_doys_dict(stats["seqlengths"], length_sorted_doy_dict_test)
             doys_stop = get_doy_stop(stats, doys_dict)
-            #fig_boxplot, _ = plot_boxplot(stats["targets"][:, 0], stats["t_stop"][:, 0], fig_boxplot, ax_boxplot, class_names, tmin=0, tmax=args.sequencelength)
             fig_boxplot, _ = boxplot_stopping_times(doys_stop, stats, fig_boxplot, ax_boxplot, class_names)
             wandb.log({
-                "loss": {"trainloss": trainloss, "testloss": testloss},
-                "accuracy": accuracy,
-                "precision": precision,
-                "recall": recall,
-                "fscore": fscore,
-                "kappa": kappa,
-                "earliness": earliness,
-                "classification_loss": classification_loss,
-                "earliness_reward": earliness_reward,
-                "boxplot": wandb.Image(fig_boxplot),
-                "conf_mat" : wandb.plot.confusion_matrix(probs=None,
-                        y_true=stats["targets"][:,0], preds=stats["predictions_at_t_stop"][:,0],
-                        class_names=class_names, title="Confusion Matrix")
-            })
+                    "loss": {"trainloss": trainloss, "testloss": testloss},
+                    "accuracy": accuracy,
+                    "precision": precision,
+                    "recall": recall,
+                    "fscore": fscore,
+                    "kappa": kappa,
+                    "elects_earliness": earliness,
+                    "classification_loss": classification_loss,
+                    "earliness_reward": earliness_reward,
+                    "classification_earliness": stats["classification_earliness"],
+                    "harmonic_mean": harmonic_mean,
+                    "boxplot": wandb.Image(fig_boxplot),
+                    "conf_mat" : wandb.plot.confusion_matrix(probs=None,
+                            y_true=stats["targets"][:,0], preds=stats["predictions_at_t_stop"][:,0],
+                            class_names=class_names, title="Confusion Matrix")
+                })
             plt.close(fig_boxplot)
 
             df = pd.DataFrame(train_stats).set_index("epoch")
@@ -242,8 +211,8 @@ def main(args):
                     torch.save(model.state_dict(), args.snapshot)
 
                     optimizer_snapshot = os.path.join(os.path.dirname(args.snapshot),
-                                                      os.path.basename(args.snapshot).replace(".pth", "_optimizer.pth")
-                                                      )
+                                                        os.path.basename(args.snapshot).replace(".pth", "_optimizer.pth")
+                                                        )
                     torch.save(optimizer.state_dict(), optimizer_snapshot)
                     wandb.log_artifact(args.snapshot, type="model")  
 
@@ -257,8 +226,8 @@ def main(args):
                         savemsg = ""
 
             pbar.set_description(f"epoch {epoch}: trainloss {trainloss:.2f}, testloss {testloss:.2f}, "
-                     f"accuracy {accuracy:.2f}, earliness {earliness:.2f}. "
-                     f"classification loss {classification_loss:.2f}, earliness reward {earliness_reward:.2f}. {savemsg}")
+                        f"accuracy {accuracy:.2f}, earliness {earliness:.2f}. "
+                        f"classification loss {classification_loss:.2f}, earliness reward {earliness_reward:.2f}, harmonic mean {harmonic_mean:.2f}. {savemsg}")
             
                 
             if args.patience is not None:
@@ -270,60 +239,6 @@ def main(args):
     wandb.log_artifact(args.snapshot, type="model")
 
 
-def train_epoch(model, dataloader, optimizer, criterion, device):
-    losses = []
-    model.train()
-    for batch in dataloader:
-        optimizer.zero_grad()
-        X, y_true = batch
-        X, y_true = X.to(device), y_true.to(device)
-        log_class_probabilities, probability_stopping = model(X)
-
-        loss = criterion(log_class_probabilities, probability_stopping, y_true)
-
-        if not loss.isnan().any():
-            loss.backward()
-            optimizer.step()
-
-            losses.append(loss.cpu().detach().numpy())
-
-    return np.stack(losses).mean()
-
-
-def test_epoch(model, dataloader, criterion, device):
-    model.eval()
-
-    stats = []
-    losses = []
-    slengths = []
-    for ids, batch in enumerate(dataloader):
-        X, y_true = batch
-        X, y_true = X.to(device), y_true.to(device)
-
-        seqlengths = (X[:,:,0] != 0).sum(1)
-        slengths.append(seqlengths.cpu().detach())
-
-        log_class_probabilities, probability_stopping, predictions_at_t_stop, t_stop = model.predict(X)
-        loss, stat = criterion(log_class_probabilities, probability_stopping, y_true, return_stats=True)
-
-        stat["loss"] = loss.cpu().detach().numpy()
-        stat["probability_stopping"] = probability_stopping.cpu().detach().numpy()
-        stat["class_probabilities"] = log_class_probabilities.exp().cpu().detach().numpy()
-        stat["predictions_at_t_stop"] = predictions_at_t_stop.unsqueeze(-1).cpu().detach().numpy()
-        stat["t_stop"] = t_stop.unsqueeze(-1).cpu().detach().numpy()
-        stat["targets"] = y_true.cpu().detach().numpy()
-        stat["ids"] = ids
-
-        stats.append(stat)
-
-        losses.append(loss.cpu().detach().numpy())
-
-    # list of dicts to dict of lists
-    stats = {k: np.vstack([dic[k] for dic in stats]) for k in stats[0]}
-    stats["seqlengths"] = torch.cat(slengths).numpy()
-
-    return np.stack(losses).mean(), stats
-
 
 if __name__ == '__main__':
     args = parse_args()
@@ -332,7 +247,6 @@ if __name__ == '__main__':
         project="ELECTS",
         notes="first experimentations with ELECTS",
         tags=["ELECTS", args.dataset, "with_doys_boxplot"],
-        # track hyperparameters and run metadata
         config={
         "dataset": args.dataset,
         "alpha": args.alpha,
---
+++ utils/helpers_training.py
@@ -0,0 +1,96 @@
+import argparse
+import sys
+import os 
+os.environ['MPLCONFIGDIR'] = '/myhome'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import torch
+import numpy as np
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Run ELECTS Early Classification training on the BavarianCrops dataset.')
+    parser.add_argument('--dataset', type=str, default="bavariancrops", choices=["bavariancrops","breizhcrops", "ghana", "southsudan","unitedstates"], help="dataset")
+    parser.add_argument('--alpha', type=float, default=0.5, help="trade-off parameter of earliness and accuracy (eq 6): "
+                                                                 "1=full weight on accuracy; 0=full weight on earliness")
+    parser.add_argument('--epsilon', type=float, default=10, help="additive smoothing parameter that helps the "
+                                                                  "model recover from too early classificaitons (eq 7)")
+    parser.add_argument('--learning-rate', type=float, default=1e-3, help="Optimizer learning rate")
+    parser.add_argument('--weight-decay', type=float, default=0, help="weight_decay")
+    parser.add_argument('--patience', type=int, default=30, help="Early stopping patience")
+    parser.add_argument('--device', type=str, default="cuda" if torch.cuda.is_available() else "cpu",
+                        choices=["cuda", "cpu"], help="'cuda' (GPU) or 'cpu' device to run the code. "
+                                                     "defaults to 'cuda' if GPU is available, otherwise 'cpu'")
+    parser.add_argument('--epochs', type=int, default=100, help="number of training epochs")
+    parser.add_argument('--sequencelength', type=int, default=70, help="sequencelength of the time series. If samples are shorter, "
+                                                                "they are zero-padded until this length; "
+                                                                "if samples are longer, they will be undersampled")
+    parser.add_argument('--batchsize', type=int, default=256, help="number of samples per batch")
+    parser.add_argument('--dataroot', type=str, default=os.path.join(os.environ.get("HOME", os.environ.get("USERPROFILE")),"elects_data"), help="directory to download the "
+                                                                                 "BavarianCrops dataset (400MB)."
+                                                                                 "Defaults to home directory.")
+    parser.add_argument('--snapshot', type=str, default="snapshots/model.pth",
+                        help="pytorch state dict snapshot file")
+    parser.add_argument('--resume', action='store_true')
+
+
+    args = parser.parse_args()
+
+    if args.patience < 0:
+        args.patience = None
+
+    return args
+
+
+def train_epoch(model, dataloader, optimizer, criterion, device):
+    losses = []
+    model.train()
+    for batch in dataloader:
+        optimizer.zero_grad()
+        X, y_true = batch
+        X, y_true = X.to(device), y_true.to(device)
+        log_class_probabilities, probability_stopping = model(X)
+
+        loss = criterion(log_class_probabilities, probability_stopping, y_true)
+
+        if not loss.isnan().any():
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss.cpu().detach().numpy())
+
+    return np.stack(losses).mean()
+
+
+def test_epoch(model, dataloader, criterion, device):
+    model.eval()
+
+    stats = []
+    losses = []
+    slengths = []
+    for ids, batch in enumerate(dataloader):
+        X, y_true = batch
+        X, y_true = X.to(device), y_true.to(device)
+
+        seqlengths = (X[:,:,0] != 0).sum(1)
+        slengths.append(seqlengths.cpu().detach())
+
+        log_class_probabilities, probability_stopping, predictions_at_t_stop, t_stop = model.predict(X)
+        loss, stat = criterion(log_class_probabilities, probability_stopping, y_true, return_stats=True)
+
+        stat["loss"] = loss.cpu().detach().numpy()
+        stat["probability_stopping"] = probability_stopping.cpu().detach().numpy()
+        stat["class_probabilities"] = log_class_probabilities.exp().cpu().detach().numpy()
+        stat["predictions_at_t_stop"] = predictions_at_t_stop.unsqueeze(-1).cpu().detach().numpy()
+        stat["t_stop"] = t_stop.unsqueeze(-1).cpu().detach().numpy()
+        stat["targets"] = y_true.cpu().detach().numpy()
+        stat["ids"] = ids
+
+        stats.append(stat)
+
+        losses.append(loss.cpu().detach().numpy())
+
+    # list of dicts to dict of lists
+    stats = {k: np.vstack([dic[k] for dic in stats]) for k in stats[0]}
+    stats["seqlengths"] = torch.cat(slengths).numpy()
+    stats["classification_earliness"] = np.mean(t_stop.cpu().detach().numpy()/stats["seqlengths"])
+
+    return np.stack(losses).mean(), stats
