fdf7aa378ece4085bac32c3c4d9e635c25dc137e
Anya M <anya-aurore.mauron@epfl.ch>
2024-06-04 16:33:17 +0200

    new lin regr  in daily reward

--- utils/losses/daily_reward_lin_regr_loss.py
+++ utils/losses/daily_reward_lin_regr_loss.py
@@ -2,8 +2,8 @@ import torch
 from torch import nn
 from utils.losses.loss_helpers import probability_correct_class, log_class_prob_at_t_plus_zt
 
-class DailyRewardLoss(nn.Module):
-    def __init__(self, alpha=0.5, weight=None, alpha_decay: list=None, epochs: int=100, start_decision_head_training: int=0):
+class DailyRewardLinRegrLoss(nn.Module):
+    def __init__(self, alpha:float=1., weight=None, alpha_decay: list=None, epochs: int=100, start_decision_head_training: int=0, mu: float=150.):
         """_summary_
 
         Args:
@@ -14,7 +14,7 @@ class DailyRewardLoss(nn.Module):
             epochs (int, optional): number of epochs. Defaults to 100.
             start_decision_head_training (int, optional): epoch to start training the decision head. Defaults to 0.
         """
-        super(DailyRewardLoss, self).__init__()
+        super(DailyRewardLinRegrLoss, self).__init__()
 
         self.weight = weight
         self.negative_log_likelihood = nn.NLLLoss(reduction="none", weight=self.weight)
@@ -26,6 +26,7 @@ class DailyRewardLoss(nn.Module):
             self.epochs = epochs
         
         self.start_decision_head_training = start_decision_head_training
+        self.mu = mu
             
 
     def forward(self, log_class_probabilities, timestamps_left, y_true, return_stats=False, **kwargs):
@@ -43,33 +44,37 @@ class DailyRewardLoss(nn.Module):
                 # alpha goes from alpha_decay_max to alpha_decay_min linearly
                 self.alpha = self.alpha_decay_min + (self.alpha_decay_max - self.alpha_decay_min) * \
                     (1 - (epoch-self.start_decision_head_training)/(self.epochs-self.start_decision_head_training))
-                
-        # earliness reward 
+                            
+        # earliness reward and linear regression loss
         if epoch>=self.start_decision_head_training and self.alpha<1.-1e-8:
             earliness_reward = probability_correct_class(log_class_probabilities_at_t_plus_zt, y_true, weight=self.weight) * (1-t/T) * (1-timestamps_left.float()/T)
             earliness_reward = earliness_reward.sum(1).mean(0)
+            
+            lin_regr_zt_loss = lin_regr_zt(t, T, self.mu, timestamps_left.float())
+            lin_regr_zt_loss = lin_regr_zt_loss.sum(1).mean(0)
         else:
             # if the decision head is not trained, the earliness reward is zero
             earliness_reward = torch.tensor(0.0, device=log_class_probabilities.device) 
-
-        # equation 4 left term
-        if epoch>=self.start_decision_head_training and self.alpha<1.-1e-8:
-            cross_entropy = self.negative_log_likelihood(log_class_probabilities_at_t_plus_zt.view(N*T,C), y_true.view(N*T)).view(N,T)
-        else: 
-            cross_entropy = self.negative_log_likelihood(log_class_probabilities.view(N*T,C), y_true.view(N*T)).view(N,T)
+            lin_regr_zt_loss = torch.tensor(0.0, device=log_class_probabilities.device)
+            
+        # classification loss
+        cross_entropy = self.negative_log_likelihood(log_class_probabilities.view(N*T,C), y_true.view(N*T)).view(N,T)
         classification_loss = cross_entropy.sum(1).mean(0)
 
-        loss = self.alpha * classification_loss - (1-self.alpha) * earliness_reward
+        # final loss
+        loss = self.alpha*classification_loss - (1.-self.alpha/2.)*earliness_reward + (1.-self.alpha/2.)*lin_regr_zt_loss
 
         if return_stats:
             stats = dict(
                 classification_loss=classification_loss.cpu().detach().numpy(),
                 earliness_reward=earliness_reward.cpu().detach().numpy(),
                 timestamps_left=timestamps_left.cpu().detach().numpy(),
+                lin_regr_zt_loss=lin_regr_zt_loss.cpu().detach().numpy(),
             )
             return loss, stats
         else:
             return loss
         
 
-
+def lin_regr_zt(t, T, mu, z_t):
+    return ((mu-t-z_t)/T)**2
--- utils/losses/loss_helpers.py
+++ utils/losses/loss_helpers.py
@@ -19,3 +19,19 @@ def probability_correct_class(logprobabilities, targets, weight=None):
         # for each y, in y_haty, we multiply by the weight of the class
         result = result * weight[targets]
     return result
+
+
+def log_class_prob_at_t_plus_zt(log_class_probabilities, timestamps_left):
+    """
+    INPUT:
+    - log_class_probabilities: tensor of shape (batchsize, sequencelength, nclasses)
+    - timestamps_left: tensor of shape (batchsize, sequencelength)
+    OUTPUT:
+    - log_class_prob_at_t_plus_zt: tensor of shape (batchsize, sequence_lenght, nclasses) with the log_class_probabilities at the final timestamp, 
+        i.e. t+timestamps_left, where t is the index of the log_class_probabilities.
+    """
+    t_plus_zt = torch.arange(log_class_probabilities.shape[1]).unsqueeze(0).to(log_class_probabilities.device) + timestamps_left.int() # shape (batchsize, sequencelength)
+    # if t_plus_zt is larger than the last index, set it to the last index
+    t_plus_zt = torch.min(t_plus_zt, torch.tensor(log_class_probabilities.shape[1]-1).to(t_plus_zt.device))
+    result = log_class_probabilities[torch.arange(log_class_probabilities.shape[0]).unsqueeze(1), t_plus_zt, :] # shape (batchsize, nclasses)
+    return result
