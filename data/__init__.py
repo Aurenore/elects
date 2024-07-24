5a64092814f9f629c346d92eddc3b3f2db621fff
Anya M <anya-aurore.mauron@epfl.ch>
2024-07-24 14:47:29 +0200

    removed unused data

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,1 @@
-from .bavariancrops import BavarianCrops
 from .breizhcrops import BreizhCrops, LABELS_NAMES, SELECTED_BANDS
-from data.sustainbench.sustainbenchcrop import SustainbenchCrops
-from .modiscdl import ModisCDL
